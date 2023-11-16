dd825cea7f0dbf4ccc1589f62400eb4ac6d2df28
liaozhaozhou <liaozhaozhou@ecyao.com>
2023-11-16 16:12:56 +0800

    test

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
-function foo(x,y,z){
-  this.name='zt'
-  console.log(this.a,x+y+z);
+var foo = function () {
+    console.log("foo1")
 }
+foo()
 
-const obj={
-  a:1
+var foo = function () {
+    console.log("foo2")
 }
+foo()
 
 
-Function.prototype.myBind=function(context,...args){
-  
-  if(typeof this !== 'function')  return new TypeError('is not a function')
-
-  context=context||window
-
-  let _this=this
-  
-  return function F(...arg){
-    //判断返回出去的F有没有被new，有就要把foo给到new出来的对象
-    if(this instanceof F){
-      return new _this(...args,...arg) //new一个foo
-    }
-    _this.apply(context,args.concat(arg))  //this是F的,_this是foo的  把foo的this指向obj用apply
-  }
+function foo() {
+    console.log("foo1")
 }
+foo()
 
-//验证
-const bar=foo.myBind(obj,1,2)
-console.log(new bar(3));   //undefined 6  foo { name: 'zt' }
-
+// function foo() {
+//     console.log("foo2")
+// }
+// foo()
